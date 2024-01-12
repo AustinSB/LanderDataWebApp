60232c09ae9dbda660c4a41e80bf635f2db28faf
Austin Briere <austinbriere93@gmail.com>
2024-01-12 17:15:18 -0500

    Add project files.

---
+++ LanderDataWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LanderDataWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
